b5a94c986d182fbc3c53416eee730da8434a809b
lixia <lx19890505@163.com>
2018-11-26 17:47:03 +0800

    初始化项目

---
+++ src/components/app-header/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <header class="header">
+    <h1 class="logo">宁国爱提</h1>
+    <div class="user-panel">
+      <div class="user">
+        <p>李夏</p>
+        <ul>
+          <li>我的地址</li>
+          <li>账号设置</li>
+          <li> 退出</li>
+        </ul>
+      </div>
+      <div>我的订单</div>
+    </div>
+  </header>
+</template>
+
+<style lang="stylus" scoped>
+.header
+  display flex
+  justify-content space-between
+  align-items center
+  height 40px
+  background-color #4a70c1
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <img alt="爱去宁国 logo" src="../assets/brand.png">
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'home',
+  components: {
+  }
+}
+</script>
